81f74003655506eb3461d49b3755c3f30386c490
Dongeun Paeng <dongeun.paeng@gmail.com>
2020-08-01 03:06:42 +0900

    Minor fix

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Dongeun's personal blog
 
 Not too much to explain here!
+Output Link: https://dongeun.co
--- components/intro.js
+++ components/intro.js
@@ -6,9 +6,9 @@ export default function Intro() {
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         <div style={{ textAlign: "right" }}>
-          <div>문득문득 드는 생각들</div>
-          <div>잊을 만하면 다시 떠올리기 위해서</div>
-          <div>공유하기 위해서</div>
+          <div>안녕하세요, 팽동은입니다.</div>
+          <div>문득 드는 생각들이 사라지는 게 아쉬워서</div>
+          <div>옳은 생각과 그른 생각 모두 남겨봅니다 :)</div>
         </div>
       </h4>
     </section>
