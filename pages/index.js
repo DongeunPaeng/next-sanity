29e0f6e74ee6f8bed4bb36507f722b8511748f2e
Dongeun Paeng <dongeun.paeng@gmail.com>
2020-12-15 00:43:41 +0900

    Modified due to the security alert

--- pages/index.js
+++ pages/index.js
@@ -34,6 +34,6 @@ export async function getStaticProps({ preview = false }) {
   const allPosts = await getAllPostsForHome(preview);
   return {
     props: { allPosts, preview },
-    revalidate: 1
-  }
+    revalidate: 1,
+  };
 }
