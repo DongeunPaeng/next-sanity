7165e86fb9ae12d99fcfd3b50a4f0f2304c5e30b
Dongeun Paeng <dongeun.paeng@gmail.com>
2021-02-03 21:26:11 +0900

    Added 'add to slack' button

--- components/header.js
+++ components/header.js
@@ -5,16 +5,16 @@ export default function Header() {
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/">
         <a className="hover:underline">Dongeun Paeng</a>
+        <a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook&client_id=476647452452.1338088550660">
+          <img
+            alt="Add to Slack"
+            height="40"
+            width="139"
+            src="https://platform.slack-edge.com/img/add_to_slack.png"
+            srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+          />
+        </a>
       </Link>
-      <a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook&client_id=476647452452.1338088550660">
-        <img
-          alt="Add to Slack"
-          height="40"
-          width="139"
-          src="https://platform.slack-edge.com/img/add_to_slack.png"
-          srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-        />
-      </a>
     </h2>
   );
 }
