5f790f139a5505e6cff9b54e8459e9d8ebeb6fc5
Dongeun Paeng <dongeun.paeng@gmail.com>
2021-02-09 13:35:21 +0900

    Add to Slack button removed

--- components/header.js
+++ components/header.js
@@ -6,15 +6,6 @@ export default function Header() {
       <Link href="/">
         <a className="hover:underline">Dongeun Paeng</a>
       </Link>
-      <a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook&client_id=476647452452.1338088550660">
-        <img
-          alt="Add to Slack"
-          height="40"
-          width="139"
-          src="https://platform.slack-edge.com/img/add_to_slack.png"
-          srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-        />
-      </a>
     </h2>
   );
 }
